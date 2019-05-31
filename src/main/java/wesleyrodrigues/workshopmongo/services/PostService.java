f556c53808056e591bb21619a55e85f545f432e6
Rodrizwesley <wesleylima47@gmail.com>
2019-05-31 10:49:50 -0300

    get posts by id

---
+++ src/main/java/wesleyrodrigues/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package wesleyrodrigues.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import wesleyrodrigues.workshopmongo.domain.Post;
+import wesleyrodrigues.workshopmongo.repository.PostRepository;
+import wesleyrodrigues.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+
+	}
+}
