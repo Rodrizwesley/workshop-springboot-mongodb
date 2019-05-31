80f0e6b9bf759df4abff5da4c96c1ad8a3d50fe9
Rodrizwesley <wesleylima47@gmail.com>
2019-05-31 11:05:17 -0300

    add comments to post's

--- src/main/java/wesleyrodrigues/workshopmongo/domain/Post.java
+++ src/main/java/wesleyrodrigues/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package wesleyrodrigues.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import wesleyrodrigues.workshopmongo.dto.AuthorDTO;
+import wesleyrodrigues.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {	
 	}
@@ -70,6 +75,13 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -94,5 +106,5 @@ public class Post implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 }
